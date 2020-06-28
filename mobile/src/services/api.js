14c4e1de61817afa98e7654e8598109a4b8c5b4e
sibelly <sibellychan@hotmail.com>
2020-06-28 18:35:00 -0400

    Adds like repository functionality in mobile and tests

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,16 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseUrl: 'http://192.168.0.16:3333'
-})
+  baseURL: "http://192.168.0.16:3333",
+});
 
-export default api
-
-/**
- * iOS com Emulador: localhost
- * iOS com dispositivo físico: IP da máquina
- * Android com Emulador: localhost (adb reverse tcp:3333 tcp:3333)
- * Android com Emulador: 10.0.2.2 (Android Studio)
- * Android com Emulador: 10.0.3.2 (Genymotion)
- * Android com dispositivo físico: IP da máquina
- */
+export default api;
